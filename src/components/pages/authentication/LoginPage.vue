33f2fe1123d5e2f2d8f2d492bf77f8513bb59f71
Vincent <vleclerc@inow.fr>
2021-11-08 17:09:59 +0100

    form niv1

--- src/components/pages/authentication/LoginPage.vue
+++ src/components/pages/authentication/LoginPage.vue
@@ -1,9 +1,40 @@
 <template>
     <h1>Connexion</h1>
+    <form @submit.prevent="submit">
+        <div>
+            <span class="p-float-label">
+                <InputText id="email" type="email" />
+                <label for="email">Login</label>
+            </span>
+        </div>
+        <div>
+            <span class="p-float-label">
+                <InputText id="password" type="password" />
+                <label for="password">Mot de passe</label>
+            </span>
+        </div>
+
+        <div>
+            <Button type="submit">Se connecter</button>
+        </div>
+    </form>
 </template>
-<script setup></script>
+<script setup>
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+</script>
 <script>
 export default {
-    
+    components:{
+        Button,
+        InputText
+    },
+    methods:{
+        submit(){
+            //ev.preventDefault();
+            
+            console.log("test");
+        }
+    }
 }
 </script>
