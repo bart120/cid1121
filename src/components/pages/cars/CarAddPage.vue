3afb433dd514d4e5d627cb3df2cf678970ea5e3e
Vincent <vleclerc@inow.fr>
2021-11-10 09:58:23 +0100

    form & calendar

--- src/components/pages/cars/CarAddPage.vue
+++ src/components/pages/cars/CarAddPage.vue
@@ -1,9 +1,35 @@
 <template>
     <h1>Ajouter une voiture</h1>
+    <form novalidate>
+        <div>
+            <input-cid label="Modèle" type="text" v-model="car.model" />
+        </div>
+        <div>
+            <input-cid label="Prix" type="number" v-model="car.price" />
+        </div>
+        <div>
+            <label>Mise en circulation</label>
+            <Calendar v-model="car.dateOfCirculation" />
+        </div>
+        <div>
+            <Button type="submit">Enregistrer</Button>
+        </div>
+    </form>
 </template>
-<script setup></script>
+<script setup>
+    import Button from 'primevue/button';
+    import InputCid from '../../core/input/InputCid.vue';
+    import Calendar from 'primevue/calendar';
+</script>
 <script>
 export default {
-    
+    components:{
+        Button,
+        InputCid,
+        Calendar
+    },
+    data(){
+        return {car:{}};
+    }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import 'primevue/resources/primevue.min.css';
 import 'primevue/resources/themes/saga-blue/theme.css';
-
+import PrimeVue from 'primevue/config';
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from './conf/routes';
+import loc from './conf/locale';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -13,4 +14,5 @@ const router = createRouter({
 
 createApp(App)
     .use(router)
+    .use(PrimeVue, loc)
     .mount('#app');
